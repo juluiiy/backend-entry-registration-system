58526459939b7bbc5d2047acd2aabda9eed328c6
Yulia Feketa <yulia.feketa@gmail.com>
2024-05-06 16:13:20 +0300

    added registration and authorization, users and roles, security config

---
+++ src/main/java/org/backendentryregistrationsystem/controller/AuthController.java
@@ -0,0 +1,2 @@
+package org.backendentryregistrationsystem.controller;public class AuthController {
+}
---
+++ src/main/java/org/backendentryregistrationsystem/security/SecurityConfig.java
@@ -0,0 +1,2 @@
+package org.backendentryregistrationsystem.security;public class SecurityConfig {
+}
