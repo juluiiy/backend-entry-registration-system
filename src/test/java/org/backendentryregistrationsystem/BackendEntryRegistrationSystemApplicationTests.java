acb5d6ab466029b26ba8b17ddf9d736ae9f53768
Yulia Feketa <yulia.feketa@gmail.com>
2024-05-06 10:32:47 +0300

    initial commit

---
+++ src/test/java/org/backendentryregistrationsystem/BackendEntryRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backendentryregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEntryRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
